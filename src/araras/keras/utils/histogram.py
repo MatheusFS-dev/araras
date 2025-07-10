b7f437f3e3635308a27f6cbb8cfd200e7eea0902
MatheusFS-dev <matheusferreiravga@gmail.com>
2025-07-10 15:08:40 -0300

    update

--- src/araras/keras/utils/histogram.py
+++ src/araras/keras/utils/histogram.py
@@ -89,7 +89,7 @@ def model_param_distribution(
 
     print()
 
-    fig, axes = plt.subplots(1, 3, figsize=(18, 8))
+    fig, axes = plt.subplots(1, 3, figsize=(24, 8))
     axes[0].hist(param_counts, bins=100, color="black")
     axes[0].set_xlabel("Number of parameters")
     axes[0].set_ylabel("Frequency")
