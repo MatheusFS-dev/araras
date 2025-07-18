7bb0d08337b7f0e1dcbe42f5afb679dde2125f7a
MatheusFS-dev <matheusferreiravga@gmail.com>
2025-07-18 14:53:35 -0300

    Update

--- src/araras/core.py
+++ src/araras/core.py
@@ -82,7 +82,7 @@ class UTCFormatter(ColorFormatter):
         from datetime import datetime, timezone
         dt = datetime.fromtimestamp(record.created, tz=timezone.utc).astimezone()
         utc_offset = dt.strftime("%z")
-        utc_offset_formatted = f"{utc_offset[:3]}:{utc_offset[3:]}"  # Format as ±HH:MM
+        utc_offset_formatted = int(utc_offset[:3])  # Extract and convert to integer (±HH)
         return f"{dt.strftime('%Y-%m-%d %H:%M:%S')} {utc_offset_formatted}"
 
 logger_time = make_logger(
