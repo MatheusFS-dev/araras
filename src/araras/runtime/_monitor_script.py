6ea445ad3715ccf5d2d2158beabac6218b83a73c
MatheusFS-dev <matheusferreiravga@gmail.com>
2025-07-24 07:19:30 -0300

    fix: change default behavior for suppressing TensorFlow warnings

--- src/araras/runtime/_monitor_script.py
+++ src/araras/runtime/_monitor_script.py
@@ -105,7 +105,7 @@ def _parse_args(argv: Optional[List[str]] = None) -> argparse.Namespace:
         "-w",
         "--supress-tf-warnings",
         action="store_true",
-        default=True,
+        default=False,
         help="Suppress TensorFlow warnings",
     )
     parser.add_argument(
